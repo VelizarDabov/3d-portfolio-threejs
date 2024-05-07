418b86cf854e260c18fc5605676735fd6d118b70
Velizar Dabov <velizardabov@gmail.com>
2024-05-07 19:20:52 +0300

    about section ready

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-
+import { skills, experiences } from "../constants";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import CTA from "../components/CTA";
 const About = () => {
   return (
     <section className="max-container">
@@ -18,10 +24,87 @@ const About = () => {
       </div>
       <div className="py-10 flex flex-col">
         <h3 className="subhead-text">My skills</h3>
-<div className="mt-16 flex flex-wrap gap-12">
-
-</div>
+        <div className="mt-16 flex flex-wrap gap-12">
+          {skills.map((skill, index) => (
+            <div
+              key={index}
+              className="block-container w-20 h-20 cursor-pointer"
+            >
+              <div className="btn-back rounded-xl" />
+              <div className="btn-font rounded-xl flex justify-center items-center">
+                <img
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
+      <div className="py-16">
+        <h3 className="subhead-text">Work Experience</h3>
+        <div className="mt-5 flex flex-col gap-3 text-slate-500 ">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
+            enim laborum aliquid minus praesentium neque consectetur aspernatur
+            nostrum ipsam. Dolor.
+          </p>
+        </div>
+        <div className="mt-12 flex">
+          <VerticalTimeline>
+            {experiences.map((experience) => (
+              <VerticalTimelineElement
+                key={experience.company_name}
+                date={experience.date}
+                icon={
+                  <div className="flex justify-center items-center w-full h-full">
+                    <img
+                      src={experience.icon}
+                      alt={experience.company_name}
+                      className="w-[60%] h-[60%] object-contain"
+                    />
+                  </div>
+                }
+                contentStyle={{
+                  borderBottom: "8px",
+                  borderStyle: "solid",
+                  borderBottomColor: experience.iconBg,
+                }}
+                iconStyle={{
+                  borderBottom: "8px",
+                  borderStyle: "solid",
+                  borderBottomColor: experience.iconBg,
+                }}
+              >
+                <div>
+                  <h3 className="text-xl text-black font-poppins font-semibold">
+                    {experience.title}
+                  </h3>
+                  <p
+                    className="text-black-500 font-medium font-base"
+                    style={{ margin: 0 }}
+                  >
+                    {experience.company_name}
+                  </p>
+                </div>
+                <ul className="my-5 list-disc ml-5 space-y-2">
+                  {experience.points.map((point, index) => (
+                    <li
+                      key={index}
+                      className="text-black-500/50 font-normal pl-1 text-sm"
+                    >
+                      {point}
+                    </li>
+                  ))}
+                </ul>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+      </div>
+      <hr className="border-slate-200"/>
+      <CTA />
     </section>
   );
 };
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
-
+import { Link } from "react-router-dom";
+import { projects } from "../constants";
+import { arrow } from "../assets/icons";
+import CTA from "../components/CTA";
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <section className="max-container">
+      <h1 className="head-text">
+        My{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Projects
+        </span>
+      </h1>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500 ">
+        <p className="">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
+          enim laborum aliquid minus praesentium neque consectetur aspernatur
+          nostrum ipsam. Dolor.
+        </p>
+      </div>
+      <div className="flex flex-wrap my-20 gap-16">
+        {projects.map((project, index) => (
+          <div key={index} className="lg:w-[400px] w-full">
+            <div className="block-container w-12 h-12">
+              <div className={` btn-back rounded-xl ${project.theme}`} />
+              <div className="btn-front rounded-xl flex justify-center items-center">
+                <img
+                  src={project.iconUrl}
+                  alt="project icon"
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            </div>
+            <div className="mt-5 flex flex-col">
+              <h4 className="text-2xl font-popins font-semibold">
+                {project.name}
+              </h4>
+              <p className="mt-2 text-slate-500">{project.description}</p>
+              <div className="mt-5 flex items-center gap-2 font-poppins">
+                <Link
+                  to={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="font-semibold text-blue-600"
+                >
+                  Live Link
+                </Link>
+                <img
+                  src={arrow}
+                  className="w-4 h-4 object-contain"
+                  alt="arrow"
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <hr className="border-slate-200" />
+      <CTA />
+    </section>
+  );
+};
 
-export default Projects
+export default Projects;
